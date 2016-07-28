427775fcd7ec707a1352c1fadf27ef573268bcee
garima <garima8jun@gmail.com>
2016-07-28 13:01:39 +0530

    admin dashboard change

--- application/modules/admin/views/index.php
+++ application/modules/admin/views/index.php
@@ -23,86 +23,86 @@
 
 
                       <div class="row">
-<
+
                          <div class="col-md-12 col-sm-12 box0 " style="margin-left: 50px";>
 
                         <div class="box1 box2">
                   <span class="li_cloud"></span>
-                  <div class="row">
+                  <div >
                   <div class="col-md-6">
-                   <h4 class="pull-left" style="padding-top: 50px";>Completed</h4></div> </div>
+                   <h4 class="pull-left">Completed</h4></div> </div>
 
-<div class="row">
+               <div class="row">
                   <div class="col-md-6 pull-right">
                    <h5><?php echo $counter['completed']; ?></h5>
                  </div></div>
                         </div>
                   
-                      </div></div>
+                      </div></div><br>
 
-              
-                       <div class="row">
-<
-                         <div class="col-md-12 col-sm-12 box0" style="margin-left: 50px";>
+             
+
+                      <div class="row">
+
+                         <div class="col-md-12 col-sm-12 box0 " style="margin-left: 50px";>
 
                         <div class="box1 box2">
                   <span class="li_cloud"></span>
-                  <div class="row">
+                  <div >
                   <div class="col-md-6">
-                   <h4 class="pull-left">Pending</h4></div> </div>
+                    <h4 class="pull-left">Pending</h4></div> </div>
 
-<div class="row">
+               <div class="row">
                   <div class="col-md-6 pull-right">
-                   <h5><?php echo $counter['pending']; ?></h5>
+                    <h5><?php echo $counter['pending']; ?></h5>
                  </div></div>
                         </div>
                   
-                      </div></div>
+                      </div></div><br>
 
-                   
-                    <div class="row">
-<
-                         <div class="col-md-12 col-sm-12 box0" style="margin-left: 50px";>
+
+
+                      <div class="row">
+
+                         <div class="col-md-12 col-sm-12 box0 " style="margin-left: 50px";>
 
                         <div class="box1 box2">
                   <span class="li_cloud"></span>
-                  <div class="row">
+                  <div >
                   <div class="col-md-6">
                    <h4 class="pull-left">Cancelled</h4></div> </div>
 
-<div class="row">
+               <div class="row">
                   <div class="col-md-6 pull-right">
-                 <h5><?php echo $counter['cancelled']; ?></h5>
+                        <h5><?php echo $counter['cancelled']; ?></h5>
                  </div></div>
                         </div>
                   
-                      </div></div>
+                      </div></div><br>
 
-                 
-                    <div class="row">
-<
-                         <div class="col-md-12 col-sm-12 box0" style="margin-left: 50px";>
+
+
+                      <div class="row">
+
+                         <div class="col-md-12 col-sm-12 box0 " style="margin-left: 50px";>
 
                         <div class="box1 box2">
                   <span class="li_cloud"></span>
-                  <div class="row">
+                  <div >
                   <div class="col-md-6">
-                   <h4 class="pull-left">Payment Done</h4></div> </div>
+                    <h4 class="pull-left">Payment Done</h4></div> </div>
 
-<div class="row">
+               <div class="row">
                   <div class="col-md-6 pull-right">
-                 <h5><?php echo $counter['payment_done']; ?></h5>
+                        <h5><?php echo $counter['payment_done']; ?></h5>
                  </div></div>
                         </div>
                   
-                      </div></div>
+                      </div></div><br>
 
 
-                  
 
-                      
-
-
+                   
 
 
                         </div>
