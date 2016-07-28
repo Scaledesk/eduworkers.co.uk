a4f10e0f482ae76e4b588bc3992aff0dd13b841d
garima <garima8jun@gmail.com>
2016-07-28 12:26:14 +0530

    Admin Dashboard Change

--- application/modules/admin/views/index.php
+++ application/modules/admin/views/index.php
@@ -6,7 +6,7 @@
                   
                   	<div class="row mtbox">
                   		<a href="<?php echo base_url().'admin/getUsers/' ?>"><div class="col-md-2 col-sm-2 col-md-offset-1 box0">
-                  			<div class="box1">
+                  			<div class="box1 box2">
 					  			<span class="li_heart"></span>
 					  			<h4>Users Details</h4>
                   			</div>
@@ -14,47 +14,99 @@
                   		</div></a>
 
                   		<a href="<?php echo base_url().'admin/product/' ?>"><div class="col-md-2 col-sm-2 box0">
-                  			<div class="box1">
+                  			<div class="box1 box2">
 					  			<span class="li_cloud"></span>
-								<h4>Order Management</h4>
+								<h4 >Order Management</h4>
                   			</div>
 					  			
-                  		</div> </a>
+                  		</div> </a>    </div>
 
-                         <div class="col-md-2 col-sm-2 box0" style="margin-left: 50px";>
-                        <div class="box1">
+
+                      <div class="row">
+<
+                         <div class="col-md-12 col-sm-12 box0 " style="margin-left: 50px";>
+
+                        <div class="box1 box2">
                   <span class="li_cloud"></span>
-                   <h4>Completed</h4> <h5><?php echo $counter['completed']; ?></h5>
+                  <div class="row">
+                  <div class="col-md-6">
+                   <h4 class="pull-left" style="padding-top: 50px";>Completed</h4></div> </div>
+
+<div class="row">
+                  <div class="col-md-6 pull-right">
+                   <h5><?php echo $counter['completed']; ?></h5>
+                 </div></div>
                         </div>
                   
-                      </div>
+                      </div></div>
 
-                      <div class="col-md-2 col-sm-2 box0" style="margin-left: 30px";>
-                        <div class="box1">
+              
+                       <div class="row">
+<
+                         <div class="col-md-12 col-sm-12 box0" style="margin-left: 50px";>
+
+                        <div class="box1 box2">
                   <span class="li_cloud"></span>
-                   <h4>Pending</h4> <h5><?php echo $counter['pending']; ?></h5>
+                  <div class="row">
+                  <div class="col-md-6">
+                   <h4 class="pull-left">Pending</h4></div> </div>
+
+<div class="row">
+                  <div class="col-md-6 pull-right">
+                   <h5><?php echo $counter['pending']; ?></h5>
+                 </div></div>
                         </div>
                   
-                      </div> 
-                      <div class="col-md-2 col-sm-2 box0" style="margin-left: 50px";>
-                        <div class="box1">
+                      </div></div>
+
+                   
+                    <div class="row">
+<
+                         <div class="col-md-12 col-sm-12 box0" style="margin-left: 50px";>
+
+                        <div class="box1 box2">
                   <span class="li_cloud"></span>
-                   <h4>Cancelled</h4> <h5><?php echo $counter['cancelled']; ?></h5>
+                  <div class="row">
+                  <div class="col-md-6">
+                   <h4 class="pull-left">Cancelled</h4></div> </div>
+
+<div class="row">
+                  <div class="col-md-6 pull-right">
+                 <h5><?php echo $counter['cancelled']; ?></h5>
+                 </div></div>
                         </div>
                   
-                      </div>
+                      </div></div>
 
-                   <div class="col-md-2 col-sm-2 box0" style="margin-left: 50px";>
-                        <div class="box1">
+                 
+                    <div class="row">
+<
+                         <div class="col-md-12 col-sm-12 box0" style="margin-left: 50px";>
+
+                        <div class="box1 box2">
                   <span class="li_cloud"></span>
-                   <h4>Payment Done</h4> <h5><?php echo $counter['payment_done']; ?></h5>
+                  <div class="row">
+                  <div class="col-md-6">
+                   <h4 class="pull-left">Payment Done</h4></div> </div>
+
+<div class="row">
+                  <div class="col-md-6 pull-right">
+                 <h5><?php echo $counter['payment_done']; ?></h5>
+                 </div></div>
                         </div>
                   
-                      </div>
-                  		
+                      </div></div>
+
+
+                  
+
+                      
+
+
+
 
                         </div>
                         </div>
-                        </div>
+                    
                         </section>
                         </section>
